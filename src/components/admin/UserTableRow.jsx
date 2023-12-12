db3d4a92848af52d611b6c23a5a6f46427bd8fa2
Alexandretta <alxsalazar21@gmail.com>
2023-12-12 14:41:19 -0500

    unificando el codigo

--- src/components/admin/UserTableRow.jsx
+++
@@ -1,9 +0,0 @@
-export function UserTableRow({id, email, token}) {
-  return (
-    <tr>
-      <td>{id}</td>
-      <td>{email}</td>
-      <td>{token}</td>
-    </tr>
-  );
-}
--- src/pages/admin/UserList.jsx
+++ src/pages/admin/UserList.jsx
@@ -1,11 +1,11 @@
 // UsersPage
-import { AdminDataTable } from "../../components/admin/AdminDataTable";
+import { AdminUsersList } from "../../components/admin/AdminUsersList";
 
 export function UserList() {
 
   return (
     <div className='container my-4 w-50'>
-        <AdminDataTable />
+        <AdminUsersList />
     </div>
   );
 }
